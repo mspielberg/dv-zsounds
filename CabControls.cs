d09c9179008038af211cde85222ccf08266a5f90
mspielberg <9729801+mspielberg@users.noreply.github.com>
2020-12-31 12:31:04 -0800

    Increase damping on DE6 horn lever

---
+++ CabControls.cs
@@ -0,0 +1,23 @@
+using DV.CabControls;
+using DV.CabControls.Spec;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DvMod.ZSounds
+{
+    public static class AdjustCabControls
+    {
+        [HarmonyPatch(typeof(ControlsInstantiator), nameof(ControlsInstantiator.Spawn))]
+        public static class SpawnPatch
+        {
+            public static void Prefix(ControlSpec spec)
+            {
+                if (spec.name == "C horn" && spec is Lever leverSpec)
+                {
+                    if (leverSpec.jointDamper == 3) // DE6
+                        leverSpec.jointDamper = 4;
+                }
+            }
+        }
+    }
+}
