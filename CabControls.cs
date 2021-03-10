de391fb638a39831aa4157b3acf32d68ffe510e1
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-03-10 13:56:56 -0800

    Add horn quilling Force DE6 horn lever to pull only

--- CabControls.cs
+++ CabControls.cs
@@ -1,6 +1,7 @@
 using DV.CabControls;
 using DV.CabControls.Spec;
 using HarmonyLib;
+using System.Collections;
 using UnityEngine;
 
 namespace DvMod.ZSounds
@@ -12,11 +13,27 @@ namespace DvMod.ZSounds
         {
             public static void Prefix(ControlSpec spec)
             {
-                if (spec.name == "C horn" && spec is Lever leverSpec)
-                {
-                    if (leverSpec.jointDamper == 3) // DE6
-                        leverSpec.jointDamper = 5f;
-                }
+                if (spec.name == "C horn" && spec is Lever leverSpec && TrainCar.Resolve(spec.gameObject).carType == TrainCarType.LocoDiesel)
+                    leverSpec.jointLimitMin = 0;
+            }
+        }
+
+        [HarmonyPatch(typeof(DieselDashboardControls), nameof(DieselDashboardControls.Init))]
+        public static class DieselDashboardControlsInitPatch
+        {
+            public static void Postfix(DieselDashboardControls __instance)
+            {
+                __instance.StartCoroutine(ResetHornCallback(__instance));
+            }
+
+            private static IEnumerator ResetHornCallback(DieselDashboardControls __instance)
+            {
+                while ((__instance.hornControl?.ValueChanged?.GetInvocationList()?.Length ?? 0) == 0)
+                    yield return null;
+
+                var hornControl = __instance.hornControl!;
+                hornControl.ValueChanged = null;
+                hornControl.ValueChanged += e => __instance.horn.SetInput(e.newValue);
             }
         }
     }
---
+++ HornQuilling.cs
@@ -0,0 +1,37 @@
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DvMod.ZSounds
+{
+    public static class HornQuilling
+    {
+        private static readonly Dictionary<Horn, float> lastReset = new Dictionary<Horn, float>();
+
+        [HarmonyPatch(typeof(Horn), nameof(Horn.InitializeAudio))]
+        public static class HornInitializeAudioPatch
+        {
+            public static void Postfix(Horn __instance)
+            {
+                __instance.loopLayered.minPitch = 0.97f;
+            }
+        }
+
+        [HarmonyPatch(typeof(Horn), nameof(Horn.Update))]
+        public static class HornUpdatePatch
+        {
+            public static bool Prefix(Horn __instance)
+            {
+                if (__instance.input < 0.1f)
+                    lastReset[__instance] = Time.time;
+                else if (!__instance.hitPlayed && __instance.input >= 0.9f && Time.time - lastReset[__instance] < 0.5f && __instance.hit != null)
+                {
+                    __instance.hit.Play();
+                    __instance.hitPlayed = true;
+                }
+                __instance.loopLayered.Set(__instance.input);
+                return false;
+            }
+        }
+    }
+}
