2d2fd23d9e332a35c4aa76113563f2e9a88129f4
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-05-14 20:11:36 -0700

    Fix transpiler for engine fades

--- EngineFade.cs
+++ EngineFade.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using UnityEngine;
 
 namespace DvMod.ZSounds
 {
@@ -60,28 +61,40 @@ namespace DvMod.ZSounds
         [HarmonyPatch]
         public static class EngineAudioHandlePatch
         {
-            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            public static IEnumerable<CodeInstruction> GenerateNewMethod(MethodBase original, List<CodeInstruction> instructions)
             {
-                var iter = instructions.GetEnumerator();
-                IEnumerable<CodeInstruction> ReplaceNext(float value, string methodName)
-                {
-                    while (iter.MoveNext())
-                    {
-                        if (iter.Current.Is(OpCodes.Ldc_R4, value))
-                        {
-                            yield return new CodeInstruction(OpCodes.Ldloc_1) { labels = iter.Current.labels };
-                            yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(EngineFade), methodName));
-                            yield break;
-                        }
-                        yield return iter.Current;
-                    }
-                }
-                foreach (var inst in ReplaceNext(0.1f, nameof(EngineFade.GetFadeOutStart))) yield return inst;
-                foreach (var inst in ReplaceNext(0.15f, nameof(EngineFade.GetFadeInStart))) yield return inst;
-                foreach (var inst in ReplaceNext(1f, nameof(EngineFade.GetFadeOutDuration))) yield return inst;
-                foreach (var inst in ReplaceNext(2f, nameof(EngineFade.GetFadeInDuration))) yield return inst;
-                while (iter.MoveNext())
-                    yield return iter.Current;
+                var index = instructions.FindIndex(ci => ci.Is(OpCodes.Ldfld, AccessTools.Field(original.DeclaringType, "engineTurnedOn")));
+                index = instructions.FindIndex(index + 1, ci => ci.Is(OpCodes.Ldfld, AccessTools.Field(original.DeclaringType, "engineTurnedOn")));
+
+                index += 3;
+                instructions.RemoveRange(index, 4);
+                instructions.Insert(index, CodeInstruction.Call(typeof(EngineFade), nameof(EngineFade.GetFadeOutStart)));
+
+                index += 3;
+                instructions.RemoveRange(index, 4);
+                instructions.Insert(index, CodeInstruction.Call(typeof(EngineFade), nameof(EngineFade.GetFadeInStart)));
+
+                index = instructions.FindIndex(index, ci => ci.Is(OpCodes.Ldfld, AccessTools.Field(original.DeclaringType, "engineTurnedOn")));
+
+                index += 2;
+                instructions[index].opcode = OpCodes.Ldloc_1;
+                instructions[index].operand = null;
+                instructions.Insert(index + 1,  CodeInstruction.Call(typeof(EngineFade), nameof(EngineFade.GetFadeOutDuration)));
+
+                index += 3;
+                instructions[index].opcode = OpCodes.Ldloc_1;
+                instructions[index].operand = null;
+                instructions.Insert(index + 1, CodeInstruction.Call(typeof(EngineFade), nameof(EngineFade.GetFadeInDuration)));
+
+                return instructions;
+            }
+
+            public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions)
+            {
+                var before = new List<CodeInstruction>(instructions);
+                var after = GenerateNewMethod(original, before).ToList();
+                // Main.DebugLog(() => $"\nBefore:\n{string.Join("\n", before)}\n\nAfter:\n{string.Join("\n", after)}");
+                return after;
             }
 
             public static IEnumerable<MethodBase> TargetMethods()
